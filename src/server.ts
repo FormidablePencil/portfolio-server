f99f4eb024cbfa3c3118c9a95614bb3dd77c143d
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2020-10-11 10:36:47 -0700

    Heroku

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { retrieveWhileTechCollection, retrieveWholeProjectCollection, retriveAll
 import cors from 'cors'
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 
